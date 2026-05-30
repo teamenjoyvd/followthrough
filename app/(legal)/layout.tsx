2cce800e9ed6f8e6e25231b5c631bdb134c39cfc
teamenjoyvd <teamenjoyvd@gmail.com>
2026-05-30 23:28:23 +0300

    [2605-DEV-145] fix: remove (legal) route group collision

--- app/(legal)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { MarketingShell } from '@/components/marketing/MarketingShell'
-
-export default function LegalLayout({ children }: { children: React.ReactNode }) {
-  return <MarketingShell>{children}</MarketingShell>
-}
